85279c9b2ac90946673f22065812ce4fa1bdc9b9
Sergey Maksimenko <serega.maksimenko@gmail.com>
2020-12-20 14:16:45 +0600

    start create app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="user-list" v-for="obj in myJson" :key="obj.id">
+      <CardUser :user="obj"></CardUser>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import json from './assets/data.json'
+import CardUser from './components/CardUser'
 
 export default {
+  data () {
+    return {
+      myJson: json
+    }
+  },
   name: 'App',
+  created () {
+    console.log('myJson', this.myJson)
+  },
+  mounted () {
+
+  },
   components: {
-    HelloWorld
+    CardUser
   }
 }
 </script>
@@ -24,5 +37,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 700px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/CardUser.vue
@@ -0,0 +1,56 @@
+<template>
+  <article class="card-user">
+    <div class="card-user__wrapper">
+      <div class="card-user__name-ava">
+        <div class="card-user__avatar-wrap">
+          <img :src="require(`../assets/images/${user.image}.svg`)" :alt="user.name" class="card-user__avatar">
+        </div>
+        <p class="card-user__name">{{ user.name }}</p>
+      </div>
+      <p class="card-user__age"><span>{{ user.age }}</span>лет</p>
+      <p class="card-user__phone"><a href="tel:+">{{ user.phone }}</a></p>
+      <button class="card-user__favourite"></button>
+    </div>
+    <!-- Если есть видео, здесь -->
+
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['user'],
+  created () {
+    // console.log(this.$props)
+  }
+}
+</script>
+
+<style lang="scss">
+  .card-user__wrapper,
+  .card-user__name-ava {
+    display: flex;
+    align-items: center;
+
+  }
+  .card-user__name-ava {
+    min-width: 300px;
+  }
+  .card-user__avatar-wrap {
+    width: 50px;
+    height: 50px;
+    overflow: hidden;
+    border-radius: 50%;
+    background-color: #ccc;
+  }
+  .card-user__avatar-wrap {
+    margin-right: 15px;
+  }
+  .card-user__name {
+    font-size: 16px;
+    line-height: 22px;
+    color: #333;
+  }
+  .card-user__age {
+    margin-right: auto;
+  }
+</style>
